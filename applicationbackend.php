b61302c7192273afd7db9c4c4bae295742927fd3
DonMarc00 <marc.novak1@web.de>
2022-11-01 22:45:35 +0100

    Restructured and JS added

--- applicationbackend.php
+++ applicationbackend.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="appsheet.css">
     <title>Bestätigungsseite</title>
-    <link rel="stylesheet" href="appbackendsheet.css">
+    <link rel="stylesheet" href="css/appbackendsheet.css">
 </head>
 
 <body>
